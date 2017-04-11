cdd68d227f26615bcb516ede07bfb981b01356f0
bpratik013 <bpratik013@gmail.com>
2017-04-11 00:43:35 -0400

    Retrive data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {MaterialModule} from '@angular/material';
+import {RouterModule} from'@angular/router';
+import{appRoutes} from './app.routes';
 
 import { AppComponent } from './app.component';
 import { CircularComponent } from './circular/circular.component';
 import { SearchComponent } from './search/search.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CircularComponent,
-    SearchComponent
+    SearchComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/github/github.service.ts
+++ src/app/github/github.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
 import {Http} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class GithubService {
 
-  constructor(private http:Http) { }
+constructor(private http:Http) { }
 
-getUser(){
-  const searchText='js';
-  const url = 'http://api.github.com/search/users?q='+ searchText;
-  /*this.http.get(url).subscribe{
+getUser(searchText) : Observable<any>{
 
-  }*/
-}
+    const url ='http://api.github.com/search/users?q=' + searchText;
+    return this.http.get(url).map(
+    res => {
+    const data = res.json();
+    console.log(data);
+    return data;
+  }
+  )
+ }
 }
